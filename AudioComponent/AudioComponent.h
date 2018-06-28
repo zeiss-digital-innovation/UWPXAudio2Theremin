ad65386b2f1b4f2e0f8176e28e467d289f021a88
JBradon <38354620+JBradon@users.noreply.github.com>
2018-06-28 11:57:03 +0100

    Add files via upload

--- AudioComponent/AudioComponent.h
+++ AudioComponent/AudioComponent.h
@@ -3,7 +3,7 @@
 
 #define PI 3.14159265358979323846f
 #define SAMPLERATE 44100
-#define BUFFERSIZE 1024
+#define BUFFERSIZE 2048 //jb: buffersize in BYTES. It was 1024
 
 namespace AudioComponent
 {
@@ -20,7 +20,13 @@ namespace AudioComponent
 
 		private:
 
-			void FillBuffer(BYTE * buffer);
+			union byteint  //jb: used to convert a 16 bit integer to 2 bytes. (union lets us interpret the same memory both ways)
+			{
+				BYTE b[sizeof int16_t];
+				int16_t iValue;
+			};
+
+			void FillBuffer(byteint* buffer); //zz
 
 			XAUDIO2_BUFFER xaudio_buffer;
 			IXAudio2* xaudio_engine;
@@ -30,7 +36,12 @@ namespace AudioComponent
 			double currentFrequency = 440;
 			double masterVolume = 0;
 			double currentVolume = 1;
-			BYTE* data1;
-			BYTE* data2;
+			double inUseVolume =  masterVolume*currentVolume; //jb: volume being played now (only changes at crossing points where sound output is near zero) 
+			double pos = 0; //jb moved from static
+
+
+			byteint* data1; //jb: converted from BYTE to allow us to work in 16 bit integer but then convert to BYTEs
+			byteint* data2;
+
     };
 }
